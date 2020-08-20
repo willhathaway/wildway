2eb1fdecca827c25b6c7575041d0d8b57f59b05b
Will Hathaway <william.hathaway38@gmail.com>
2020-08-20 11:09:58 -0400

    site deployed. changes to mobile responsiveness on pages: Home, Journal, Portfolio

--- wildway-react/src/components/Bio/index.js
+++ wildway-react/src/components/Bio/index.js
@@ -9,6 +9,8 @@ function Bio() {
 
     let bio1 = "Welcome to WILDWAY, my personal website and portfolio. I am a 22 year old artist, designer and developer. Everything you see here was designed, drawn, painted, written and programmed by me. You will also find what I've done, a bit about who I am, and what I can do for you and your business. I build professional websites and applications for businesses and individuals, and I will work to deliver whatever you need.";
 
+    let shortBio = "A Brief Biography:";
+
     let bio2 = "I am a recent graduate of the Trilogy Coding Bootcamp at the Harvard Extension School, which ended in March 2020. I have worked on and off at InResonance Software, a company headquartered near my home in the Pioneer Valley, between 2016 and 2018. After graduating high school, I completed Division I at Hampshire College before taking a leave of absence to travel and learn. Some of the most fulfilling work I’ve done in my life has involved programming: from senior year of high school, when I used Python to map kinematic equations for an honors Physics class; to my various roles at iR; to the work that I’ve done during the bootcamp (shown on the portfolio page)."
 
     let tech = [
@@ -28,6 +30,9 @@ function Bio() {
                 <div className="bioText"><p className="boldText">{welcome}</p><br /></div>
 
                 <div className="bioText"><p>{bio1}</p></div>
+
+                <div className="bioText"><p className="boldText">{shortBio}</p><br /></div>
+
                 <div className="bioText"><p>{bio2}</p></div>
 
 
